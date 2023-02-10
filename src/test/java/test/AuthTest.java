e9f827f1b2cdb14719706e34d13f1dde004b3fdb
Vadim Mustafeev <ya.mystafeev@yandex.ru>
2023-02-10 20:41:41 +0400

    I donot know

--- src/test/java/test/AuthTest.java
+++ src/test/java/test/AuthTest.java
@@ -38,6 +38,7 @@ class AuthTest {
         $("[data-test-id='login'] input").setValue(notRegisteredUser.getLogin());
         $("[data-test-id='password'] input").setValue(notRegisteredUser.getPassword());
         $("button.button").click();
+        //$("[data-test-id='action-login']").click();
         $("[data-test-id='error-notification'] .notification__content")
                 .shouldHave(Condition.text("Ошибка! Неверно указан логин или пароль"))
                 .shouldBe((Condition.visible));
